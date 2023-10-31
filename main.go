a69c9d8b8a8604a815fa32f2df16c2757349d95f
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2023-10-31 23:17:34 +0700

    Done

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -33,33 +34,58 @@ func initRoutes() {
 
 // TODO Problem and Testing 1: Buat Fungsi yang memberikan seluruh informasi student
 func getStudents(c echo.Context) error {
-	// Tulis jawabanmu disini
+	return c.JSON(http.StatusOK, students)
 }
 
 // TODO Problem and Testing 2: Buat Fungsi yang memberikan informasi student berdasarkan ID
 func getStudent(c echo.Context) error {
-	// Tulis jawabanmu disini
+	id, _ := strconv.Atoi(c.Param("id"))
 
-	return c.JSON(http.StatusOK, students)
+	for _, student := range students {
+		if student.ID == id {
+			return c.JSON(http.StatusOK, student)
+		}
+	}
+	return c.JSON(http.StatusOK, "Student not found")
 }
 
 // TODO Problem and Testing 3: Buat Fungsi yang membuat informasi student baru
 func createStudent(c echo.Context) error {
-	// Tulis jawabanmu disini
-
+	student := new(Student)
+	if err := c.Bind(student); err != nil {
+		return err
+	}
+	student.ID = len(students) + 1
+	students = append(students, *student)
 	return c.JSON(http.StatusCreated, student)
 }
 
 // TODO Problem and Testing 4: Buat Fungsi yang dapat mengupdate informasi student berdasarkan ID
 func updateStudent(c echo.Context) error {
-	// Tulis jawabanmu disini
-
-	return c.JSON(http.StatusOK, students)
+	id, _ := strconv.Atoi(c.Param("id"))
+	for i := range students {
+		if students[i].ID == id {
+			student := new(Student)
+			if err := c.Bind(student); err != nil {
+				return err
+			}
+			students[i].Name = student.Name
+			students[i].Age = student.Age
+			students[i].Grade = student.Grade
+			return c.JSON(http.StatusOK, student)
+		}
+	}
+	return c.JSON(http.StatusOK, "Student not found")
 }
 
 // TODO Problem and Testing 5: Buat Fungsi yang dapat menghapus informasi student berdasarkan ID
 func deleteStudent(c echo.Context) error {
-	// Tulis jawabanmu disini
-
-	return c.JSON(http.StatusNoContent, students)
+	id, _ := strconv.Atoi(c.Param("id"))
+	for i := range students {
+		if students[i].ID == id {
+			students = append(students[:i], students[i+1:]...)
+			return c.JSON(http.StatusNoContent, students)
+		}
+	}
+	return c.JSON(http.StatusNoContent, "Student not found")
 }
